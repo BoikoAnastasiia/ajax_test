20f7c9f2ebeb73b2ce053590076a4b9bdaa90054
anastasyah86 <nastasyah86@gmail.com>
2021-01-31 21:19:23 +0200

    example Apis

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './styles.css';
+
+/* example with simple api */
+// const options = {
+//   method: 'GET',
+//   headers: { Accept: 'application/json' },
+// };
+// fetch('https://jsonplaceholder.typicode.com/todos', options);
+// fetch('https://jsonplaceholder.typicode.com/users').then(response =>
+//   response.json().then(console.log),
+// );
+
+/* example with difficult API   */
+// fetch('http://hn.algolia.com/api/v1/search?query=html')
+//   .then(rs => rs.json())
+//   .then(data => console.log(data));
+
+// fetch('http://hn.algolia.com/api/v1/search?query=react&tags=story')
+//   .then(rs => rs.json())
+//   .then(data => console.log(data));
+
+/* example with news API */
